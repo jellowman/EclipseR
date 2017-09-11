f934ee8fd54c7f219cc4f8a9106b5cc286595e02
jellowman <tarfisher@gmail.com>
2017-09-11 02:14:08 -0500

    Track only src
    Updated to post-lab1

--- src/EclipseR.cpp
+++ src/EclipseR.cpp
@@ -11,15 +11,16 @@
 using namespace std;
 
 
-
 int main() {
 	//Instantiate array holding used IDs to false
 	for(int i = 0; i < 20000; i++)
 	{
 		eclipseID[i] = false;
 	}
+
 	//Variable used to hold next line from the eclipse data text file
 	string nextLine;
+	//Keeps track of the line number from input file
 	int lineNum = 0;
 
 	//Read past the "junk lines" at the beginning of the file
@@ -33,63 +34,72 @@ int main() {
 	//cout << nextLine << endl;
 	//hoi
 
-	while(getline(cin, nextLine)) {
-	string columns[20];
-	lineNum++;
-	int numCols = ColumnSplitter(columns, nextLine);
-	//Check for correct # of columns based on eclipse type
-	if (columns[9].at(0) == 'P') {
-		if(numCols != 16) {
-			cerr << "INCORRECT COLUMNS for P-Type Eclipse on line: " << lineNum << endl;
+	//Loop through input file until the end of file is reached (Does not work properly, processing getline() never returns)
+	while(getline(cin, nextLine)) { //Must CTRL+Z to end program
+		lineNum++;
+
+		//string array to hold individual columns from file
+		string columns[20];
+
+		//Read nextLine, put parts into columns
+		int numCols = ColumnSplitter(columns, nextLine);
+
+		//Check for correct # of columns based on eclipse type
+		if (columns[9].at(0) == 'P') {
+			if(numCols != 16) {
+				cerr << "INCORRECT COLUMNS for P-Type Eclipse on line: " << lineNum << endl;
+			}
+		} else {
+			if(numCols != 18) {
+				cerr << "INCORRECT COLUMNS for Non-P-Type Eclipse on line: " << lineNum << endl;
+			}
 		}
-	} else {
-		if(numCols != 18) {
-			cerr << "INCORRECT COLUMNS for Non-P-Type Eclipse on line: " << lineNum << endl;
+
+		//Check for unique catalog number
+		int idNum = stoi(columns[0]);
+		if(eclipseID[idNum] == true) { //true means it's a duplicate ID
+			cerr << "DUPLICATE ID FOUND: ID no. " << idNum << endl;
+		} else {
+			//Indicate an ID has been used
+			eclipseID[idNum] = true;
+
+			//Print out in CSV format, loop except last column
+			for(int i = 0; i < numCols-1; i++) {
+				cout << columns[i] << ",";
+			}
+			cout << columns[numCols-1] << endl;
 		}
-	}
 
-	//Check for unique catalog number
-	int idNum = stoi(columns[0]);
-	if(eclipseID[idNum] == true) //true means it's a duplicate ID
-	{
-		cerr << "DUPLICATE ID FOUND: ID no. " << idNum << endl;
-	} else {
-		eclipseID[idNum] = true;
-
-		//Print out in CSV format, loop except last column
-		for(int i = 0; i < numCols-1; i++) {
-			cout << columns[i] << ",";
+		if(!cin.good()) //DOES NOT WORK, attempt to break out if end of file is reached
+		{
+			break;
 		}
-		cout << columns[numCols-1] << endl;
-	}
 
-	if(!cin.good()) //DOES NOT WORK
-	{
-		break;
-	}
-
-	//cout << "After if" << endl;
-	}
+	} //END while loop
 
 	//cout << "Done" << endl;
 
 	return 0;
-}
+} //END main() method
 
 //Function to take the whitespace separated columns in nextLine and
 //put them in individual string indexes in columns.
 int ColumnSplitter(string columns[], const string nextLine) {
 
+	//The data column being read (0 indexed)
 	int columnPos = -1;
+	//Char index in nextLine where column starts
 	int partStart = 0;
+	//Char index in nextLine where column ends
 	int partEnd = 0;
+	//Keeps track if a column or whitespace is currently traversed by char index
 	bool isWriting = false;
 
 	//Traverse the characters in nextLine
 	for (unsigned int i = 0; i < nextLine.length(); i++) {
+
 		//If the next character is a space...
 		if(nextLine.at(i) == ' ') {
-
 			if(isWriting) //Will run if the previous characters were part of a column
 			{             //Helps ignore space between columns, regardless of number of spaces
 
@@ -103,7 +113,7 @@ int ColumnSplitter(string columns[], const string nextLine) {
 				isWriting = false;
 			}
 			continue;
-		}
+		} //END checking if next char is space
 		else { //If the next char isn't a space...
 
 			if(isWriting) {
@@ -114,8 +124,8 @@ int ColumnSplitter(string columns[], const string nextLine) {
 				partStart = i;
 				isWriting=true;
 			}
-		}
-	}
+		} //END checking if character is not a space
+	} //END for loop
 
 	//Check to see if a column was still writing when the line ended
 	if(isWriting) {
@@ -124,5 +134,5 @@ int ColumnSplitter(string columns[], const string nextLine) {
 		columns[columnPos] = nextLine.substr(partStart, (partEnd-partStart+1));
 	}
 
-	return columnPos+1;
-}
+	return columnPos+1; //Return number of columns as 1-indexed
+} //END Function ColumnSplitter()
--- src/EclipseR.h
+++ src/EclipseR.h
@@ -8,6 +8,12 @@ using namespace std;
 //Array used to hold already entered eclipse IDs
 bool eclipseID[20000];
 
+
+//Main method of program, enuf said
+int main();
+
 //Function to take the whitespace separated columns in nextLine and
 //put them in individual string indexes in columns.
+//columns[] - Array to hold individual columns in their own separated index
+//nextLine  - String containing the next line from the input file to parse into columns[]
 int ColumnSplitter(string columns[], const string nextLine);
